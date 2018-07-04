07e18f5188b2b30f1a09ed6017884f58c2f4b4ab
ViperBling <qiusongtao2010@hotmail.com>
2018-07-04 12:54:36 +0800

    first commit

---
+++ machine-learning-ex2/ex2/plotData.m
@@ -0,0 +1,31 @@
+function plotData(X, y)
+%PLOTDATA Plots the data points X and y into a new figure 
+%   PLOTDATA(x,y) plots the data points with + for the positive examples
+%   and o for the negative examples. X is assumed to be a Mx2 matrix.
+
+% Create New Figure
+figure; hold on;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the positive and negative examples on a
+%               2D plot, using the option 'k+' for the positive
+%               examples and 'ko' for the negative examples.
+%
+
+% 找到y = 1的那些数据和y = 0的那些数据分别付给pos, neg
+pos = find(y == 1); neg = find(y == 0);
+
+% 以数据集的第一列为横坐标，第二列为纵坐标绘制图像
+
+plot(X(pos, 1), X(pos, 2), 'k+', 'LineWidth', 2, 'MarkerSize', 7);
+plot(X(neg, 1), X(neg, 2), 'ko', 'MarkerFaceColor', 'y', 'MarkerSize', 7);
+
+
+
+% =========================================================================
+
+
+
+hold off;
+
+end
---
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -0,0 +1,47 @@
+function p = predictOneVsAll(all_theta, X)
+%PREDICT Predict the label for a trained one-vs-all classifier. The labels 
+%are in the range 1..K, where K = size(all_theta, 1). 
+%  p = PREDICTONEVSALL(all_theta, X) will return a vector of predictions
+%  for each example in the matrix X. Note that X contains the examples in
+%  rows. all_theta is a matrix where the i-th row is a trained logistic
+%  regression theta vector for the i-th class. You should set p to a vector
+%  of values from 1..K (e.g., p = [1; 3; 1; 2] predicts classes 1, 3, 1, 2
+%  for 4 examples) 
+
+m = size(X, 1);
+num_labels = size(all_theta, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% Add ones to the X data matrix
+X = [ones(m, 1) X];
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned logistic regression parameters (one-vs-all).
+%               You should set p to a vector of predictions (from 1 to
+%               num_labels).
+%
+% Hint: This code can be done all vectorized using the max function.
+%       In particular, the max function can also return the index of the 
+%       max element, for more information see 'help max'. If your examples 
+%       are in rows, then, you can use max(A, [], 2) to obtain the max 
+%       for each row.
+%       
+
+pre = sigmoid(X * all_theta');
+
+% pre是一个5000 x 10的矩阵，每行代表着一个样本，其中每个元素表示预测出的从0～9这10个数的概率大小
+% 使用max函数获取每一行概率最大值，其对应的索引就是预测的输出。例如，第8项的概率最大，那么就预测对应样本
+% 的输出结果是数字8.
+
+[premax, indexOfmax] = max(pre, [], 2);
+
+p = indexOfmax;
+
+
+% =========================================================================
+
+
+end
